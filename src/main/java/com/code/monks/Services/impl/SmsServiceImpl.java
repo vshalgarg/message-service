4a76608738bb7774ed7956891207c7e1edab99a7
Pankaj-Dhami007 <pankajdhami@neepanlok.com>
2025-06-03 18:10:35 +0530

    update

--- src/main/java/com/code/monks/Services/impl/SmsServiceImpl.java
+++ src/main/java/com/code/monks/Services/impl/SmsServiceImpl.java
@@ -1,14 +1,24 @@
 package com.code.monks.Services.impl;
 
 import com.code.monks.Services.GenericMessageService;
+import com.code.monks.config.TwilioConfig;
 import com.code.monks.dtos.request.SmsRequestDto;
 import com.code.monks.enums.MessageTypeEnum;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class SmsServiceImpl implements GenericMessageService<SmsRequestDto> {
+    private final TwilioConfig twilioConfig;
     public void sendMessage(SmsRequestDto request) {
-        System.out.println("Sending SMS to " + request.getRecipient());
+        Message.creator(
+                new PhoneNumber(request.getRecipient()),
+                new PhoneNumber(twilioConfig.getFromNumber()),
+                request.getContent()
+        ).create();
     }
 
     public MessageTypeEnum getType() {
--- src/main/java/com/code/monks/dtos/request/SmsRequestDto.java
+++ src/main/java/com/code/monks/dtos/request/SmsRequestDto.java
@@ -1,5 +1,4 @@
 package com.code.monks.dtos.request;
 
 public class SmsRequestDto extends BaseMessageRequestDto {
-
 }
