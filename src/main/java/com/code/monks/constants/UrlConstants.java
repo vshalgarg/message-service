4c9d2b6a8a417d07ebf553925ca6406ecbe1a968
Pankaj-Dhami007 <pankajdhami@neepanlok.com>
2025-06-05 16:30:48 +0530

    change sms-service

---
+++ src/main/java/com/code/monks/constants/UrlConstants.java
@@ -0,0 +1,10 @@
+package com.code.monks.constants;
+
+public class UrlConstants {
+
+    public static final String CONTEXT = "/message";
+    public static final String BASE = CONTEXT+"/api/v1/send";
+
+    public static final String SEND_SMS = "/sms";
+    public static final String SEND_EMAIL= "/email";
+}
